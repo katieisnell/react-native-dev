f9ca866af02bbac617c9ad76b0388efb7e73370c
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-06 12:23:36 +0100

    Added initial artist profile card styling

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// Here we destructure the album from the props object
+const AlbumDetail = ({ album }) => {
+    // Now we further destructure the title, artist and thumbnail from the album
+    const { title, artist, thumbnail_image } = album;
+
     return (
         <Card>
-            <CardSection>
-                <Text>{props.album.title}</Text>
+            <CardSection>         
+                <View>
+                    <Image 
+                        source={{ uri: thumbnail_image }} 
+                        style={styles.thumbnailStyle}
+                    />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
