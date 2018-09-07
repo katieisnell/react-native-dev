c6eb9ebf05a26eacc18a82161af38a4746f54537
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-07 11:04:25 +0100

    Implemented button style

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+    const { textStyle, buttonStyle } = styles;
     return (
-        <Text>Try and click me (it won't work yet!)</Text>
+        <TouchableOpacity style={buttonStyle}>
+             <Text style={textStyle}>
+                Try and click me (it won't work yet!)
+            </Text>
+        </TouchableOpacity>
     );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007AFF',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007AFF',
+        marginLeft: 5,
+        marginRight: 5
+    }
 };
 
 export default Button;
