4a998d180492c3e67e439fd4687dd76fdb8c1f67
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-07 10:39:17 +0100

    Implemented initial button component

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 // Here we destructure the album from the props object
 const AlbumDetail = ({ album }) => {
@@ -29,12 +30,19 @@ const AlbumDetail = ({ album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image 
                     source={{ uri: image }} 
                     style={imageStyle} 
                 />
             </CardSection>
+
+            <CardSection>
+                <Button />
+            </CardSection>
+
+
         </Card>
     );
 };
---
+++ albums/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Button = () => {
+    return (
+        <Text>Try and click me (it won't work yet!)</Text>
+    );
+};
+
+export default Button;
