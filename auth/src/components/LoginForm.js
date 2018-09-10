87f4b0ff3dec3de2cf18a78e93fd8777bb2e36ea
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-10 10:29:26 +0100

    Hidden password input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
@@ -11,11 +11,20 @@ class LoginForm extends Component {
           <Input 
             placeholder={'Your email address'}
             label={'Email'}
+            secureTextEntry={false} // Don't need this line, as if we don't include the prop it will automatically be false
             value={this.state.email}
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
+        <CardSection>
+          <Input 
+            placeholder={'********'}
+            label={'Password'}
+            secureTextEntry={true}
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
         <CardSection>
           <Button>
             Log in
