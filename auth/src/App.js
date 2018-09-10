0d0c0006a0b31fad381bc7407b2177f1e6b263b6
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-10 09:44:49 +0100

    Implemented generic input component

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
 // Due to having an index.js file in common, we can import without specifying the Header file
