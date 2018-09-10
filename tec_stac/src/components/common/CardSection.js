238d9359465bf98ea17daf22a7f526e8386c2ecd
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-10 17:24:26 +0100

    Initialised Redux library and common components

---
+++ tec_stac/src/components/common/CardSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  }
+};
+
+// We now have to export an object as we are using a index.js file
+export { CardSection };
