09044d8789ed7f38b5c1e73141fc4e83d1c5ae83
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-11 16:57:58 +0100

    Implemented Redux action creator

--- tec_stac/src/components/ListItem.js
+++ tec_stac/src/components/ListItem.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
-import LibraryList from './LibraryList';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+
+    console.log(this.props);
+    
     return (
       <CardSection>
         <Text style={titleStyle}>{this.props.library.item.title}</Text>
@@ -21,4 +25,5 @@ const styles = {
   }
 };
 
-export default ListItem;
+// If do not want to mapStateToProps, pass null as 1st statement
+export default connect(null, actions)(ListItem);
