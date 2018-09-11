16456d58c22300a6d4fe09cb9a5db280ee6994b4
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-11 16:57:58 +0100

    Implemented Redux action creator

--- tec_stac/src/reducers/index.js
+++ tec_stac/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-  libraries: LibraryReducer // console.log(libraries.getState()) -> { libraries: [] }
+  libraries: LibraryReducer, // console.log(libraries.getState()) -> { libraries: [] }
+  selectedLibraryId: SelectionReducer
 });
