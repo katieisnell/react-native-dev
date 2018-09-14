bc8066c48d1a8559e4d73f33842ef4a0aadd2524
katieisnell <katie.snell@student.manchester.ac.uk>
2018-09-14 11:50:18 +0100

    Configured firebase credentials

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import firebase_credentials from './data/firebase_credentials.json';
 
 class App extends Component {
+  componentWillMount() {
+    const config = firebase_credentials;
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
